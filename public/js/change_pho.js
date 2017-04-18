2daac09355b101d41359961d70ab5c4a9acc76a0
adamchem <adam.chen@kimo.com>
2017-04-18 10:51:09 +0800

    Add files via upload

--- public/js/change_pho.js
+++ public/js/change_pho.js
@@ -1,11 +1,11 @@
 $(function(){
   var _select; //object now selected
   var _index;//number
-  var picture = [ "<img src='pic/ghost_house.jpg' width='500' height='500'>",
-                  "<img src='pic/wood_house.jpg' width='500' height='500'>",
-                  "<img src='pic/stone_house.jpg' width='500' height='500'>",
-                  "<img src='pic/gold_house.jpg' width='500' height='500'>",
-                  "<img src='pic/white_house.jpg' width='500' height='500'>"];
+  var picture = [ 'pic/ghost_house.jpg',
+                  'pic/wood_house.jpg',
+                  'pic/stone_house.jpg',
+                  'pic/gold_house.jpg',
+                  'pic/white_house.jpg'];
 
   $(".list ul li").click(function(){
     _select = $(this);//set object
@@ -15,10 +15,9 @@ $(function(){
       $(".list ul li").fadeTo("fast",1);
       _select.fadeTo("fast",0.5);
       $(".photo img").fadeOut(500,function(){
-        $(".photo img").replaceWith(picture[_index]);
+        $(".photo img").attr("src", picture[_index]);
         $(".photo img").fadeIn(500);
       });
     }
-  })
-
+  });
 });
--- public/js/show_info.js
+++ public/js/show_info.js
@@ -1,13 +1,11 @@
 $(function(){
   var key = [0,0,0,1,1];
-  var ans = [,,,,];
+  var ans = [];
   $("#data_send").click(function(){
-    ans[0] = $(".question #sex input[type='radio']:checked").val();
-    ans[1] = $(".question #name input[type='radio']:checked").val();
-    ans[2] = $(".question #age input[type='radio']:checked").val();
-    ans[3] = $(".question #food input[type='radio']:checked").val();
-    ans[4] = $(".question #color input[type='radio']:checked").val();
-    if(ans.toString() == key.toString()){
+    for(var i = 0; i < key.length; i++){
+      ans[i] = $(".question input[type='radio']:checked").eq(i).val();
+    }
+    if(ans.toString() === key.toString()){
       $(".info").fadeIn(500);
     }
   });
