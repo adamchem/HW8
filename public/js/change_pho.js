efb152aeb05268c4bd22d68222ba47f9a7a86ad5
adamchem <adam.chen@kimo.com>
2017-04-18 16:08:15 +0800

    Update change_pho.js

--- public/js/change_pho.js
+++ public/js/change_pho.js
@@ -12,13 +12,16 @@ $(function(){
     var num = $(".list ul li").index(_select);
     if(_index !== num){
       _index = num;
-      $(".list ul li").fadeTo("fast",1);
-      _select.fadeTo("fast",0.5);
-      $(".photo img").fadeOut(500,function(){
-        $(this)
-          .attr("src", picture[_index])
-          .fadeIn(500);
-      });
+      changePhoto();
     }
   });
+  function changePhoto(){
+    $(".list ul li").fadeTo("fast",1);
+      _select.fadeTo("fast",0.5);
+      $(".photo img").fadeOut(500,function(){
+        $(this).attr("src", picture[_index]).load(function(){
+          $(".photo img").fadeIn(500);
+        });   
+      });
+  };
 });
