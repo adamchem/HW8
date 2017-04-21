0ab7d883d2b6a9f34736598d16947981f460d474
adamchem <adam.chen@kimo.com>
2017-04-21 23:34:27 +0800

    Add files via upload

--- public/js/change_pho.js
+++ public/js/change_pho.js
@@ -1,23 +1,28 @@
 $(function(){
   var _select; //object now selected
   var _index;//number
-  var picture = [ 'pic/ghost_house.jpg',
-                  'pic/wood_house.jpg',
-                  'pic/stone_house.jpg',
-                  'pic/gold_house.jpg',
-                  'pic/white_house.jpg'];
+  var picture = [ '../public/pic/ghost_house.jpg',
+                  '../public/pic/wood_house.jpg',
+                  '../public/pic/stone_house.jpg',
+                  '../public/pic/gold_house.jpg',
+                  '../public/pic/white_house.jpg'];
 
-  $(".list ul li").click(function(){
+  $(".list ul li").on("click",function(){
     _select = $(this);//set object
     var num = $(".list ul li").index(_select);
     if(_index !== num){
       _index = num;
-      $(".list ul li").fadeTo("fast",1);
-      _select.fadeTo("fast",0.5);
-      $(".photo img").fadeOut(500,function(){
-        $(".photo img").attr("src", picture[_index]);
-        $(".photo img").fadeIn(500);
-      });
+      changePhoto();
     }
   });
+
+  function changePhoto(){
+    $(".list ul li").fadeTo("fast",1);
+    _select.fadeTo("fast",0.5);
+    $(".photo img").fadeOut(500,function(){
+      $(this).attr("src", picture[_index]).load(function(){
+        $(".photo img").fadeIn(500);
+      });   
+    });
+  };
 });
