c7e099d645a9624cd03531543a9b75b929106181
adamchem <adam.chen@kimo.com>
2017-04-21 23:28:31 +0800

    Add files via upload

--- public/js/change_pho.js
+++ public/js/change_pho.js
@@ -7,24 +7,17 @@ $(function(){
                   'pic/gold_house.jpg',
                   'pic/white_house.jpg'];
 
-  $(".list ul li").on("click",function(){
+  $(".list ul li").click(function(){
     _select = $(this);//set object
     var num = $(".list ul li").index(_select);
     if(_index !== num){
       _index = num;
-      changePhoto();
+      $(".list ul li").fadeTo("fast",1);
+      _select.fadeTo("fast",0.5);
+      $(".photo img").fadeOut(500,function(){
+        $(".photo img").attr("src", picture[_index]);
+        $(".photo img").fadeIn(500);
+      });
     }
   });
-  
-  function changePhoto(){
-    $(".list ul li").fadeTo("fast",1);
-    _select.fadeTo("fast",0.5);
-    $(".photo img").fadeOut(500,function(){
-      $(this).attr("src", picture[_index])
-        .load(function(){
-        $(".photo img").fadeIn(500);
-      });   
-    });
-  };
-  
 });
