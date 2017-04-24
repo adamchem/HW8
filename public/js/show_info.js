6691cb3f5025d35b2ae77392b63cb775b3f9ec72
adamchem <adam.chen@kimo.com>
2017-04-24 16:34:05 +0800

    Add files via upload

--- public/js/show_info.js
+++ public/js/show_info.js
@@ -6,7 +6,12 @@ $(function(){
       ans[i] = $(".question input[type='radio']:checked").eq(i).val();
     }
     if(ans.toString() === key.toString()){
+      $(".status").html('');
+      $(".status").html('Hellow~');
       $(".info").fadeIn(500);
+    }else{
+      $(".status").html('');
+      $(".status").html('Answer Wrong !');
     }
   });
 });
